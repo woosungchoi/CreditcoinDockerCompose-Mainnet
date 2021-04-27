c79e2eeece5e78ec7d53a40cacd65dc2f5b1dd73
woosungchoi <solut2000@gmail.com>
2021-04-27 22:56:42 +0900

    remove sleep

--- restart_node.sh
+++ restart_node.sh
@@ -1,7 +1,5 @@
 sudo docker-compose -f /root/CreditcoinDockerCompose-Mainnet/Client/docker-compose.yaml down
-sleep 5
 sudo docker-compose -f /root/CreditcoinDockerCompose-Mainnet/Server/docker-compose.yaml down
-sleep 5
 sudo docker-compose -f /root/CreditcoinDockerCompose-Mainnet/Client/docker-compose.yaml pull
 sudo docker-compose -f /root/CreditcoinDockerCompose-Mainnet/Server/docker-compose.yaml pull
 sudo docker-compose -f /root/CreditcoinDockerCompose-Mainnet/Server/docker-compose.yaml up -d
